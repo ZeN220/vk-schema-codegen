92efe440e30ffacbceca8b691cefe274a33c56f1
ZeN <artaganmalsagov07@gmail.com>
2022-11-27 17:57:53 +0300

    change arguments of cli

--- src/cli.py
+++ src/cli.py
@@ -5,28 +5,23 @@ import click
 
 @click.command()
 @click.option(
-    "-m",
-    "--methods-dir",
-    show_default=True,
-    default="methods",
-    help="Directory containing API methods in the form of Python functions.",
-    type=click.Path(exists=True, file_okay=False, path_type=Path),
+	"-e",
+	"--errors",
+	show_default=True,
+	default="errors.json",
+	help=(
+		"Path to the file with errors of API "
+		"(https://github.com/VKCOM/vk-api-schema/blob/master/errors.json)"
+	),
+	type=click.Path(exists=True, dir_okay=False, path_type=Path),
 )
 @click.option(
-    "-r",
-    "--responses-dir",
-    show_default=True,
-    default="responses",
-    help="Directory containing responses of API methods in the form of Python classes.",
-    type=click.Path(exists=True, file_okay=False, path_type=Path),
-)
-@click.option(
-    "-o",
-    "--objects-dir",
-    show_default=True,
-    default="objects",
-    help="Directory containing objects of API in the form of Python classes.",
-    type=click.Path(exists=True, file_okay=False, path_type=Path),
+	"-o",
+	"--output-dir",
+	show_default=True,
+	default="output",
+	help="Directory to save the output files",
+	type=click.Path(exists=True, file_okay=False, path_type=Path),
 )
 def cli():
-    pass
+	pass
