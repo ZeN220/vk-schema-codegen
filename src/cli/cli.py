cb247e33c6c84cde9cc7b166bc0ed6fe0eeae0d4
ZeN <artaganmalsagov07@gmail.com>
2022-11-28 14:34:25 +0300

    tabs are replaced by spaces

--- src/cli/cli.py
+++ src/cli/cli.py
@@ -6,40 +6,40 @@ import click
 
 @click.group()
 @click.option(
-	"-e",
-	"--errors",
-	show_default=True,
-	default="errors.json",
-	help=(
-		"Path to the file with errors of API "
-		"(https://github.com/VKCOM/vk-api-schema/blob/master/errors.json)"
-	),
-	type=click.Path(exists=True, dir_okay=False, path_type=Path),
+    "-e",
+    "--errors",
+    show_default=True,
+    default="errors.json",
+    help=(
+        "Path to the file with errors of API "
+        "(https://github.com/VKCOM/vk-api-schema/blob/master/errors.json)"
+    ),
+    type=click.Path(exists=True, dir_okay=False, path_type=Path),
 )
 @click.option(
-	"-o",
-	"--output-dir",
-	show_default=True,
-	default="output",
-	help="Directory to save the output files",
-	type=click.Path(exists=True, file_okay=False, path_type=Path),
+    "-o",
+    "--output-dir",
+    show_default=True,
+    default="output",
+    help="Directory to save the output files",
+    type=click.Path(exists=True, file_okay=False, path_type=Path),
 )
 @click.option(
-	"-i",
-	"--input-dir",
-	help="Directory with the schemas of API methods",
+    "-i",
+    "--input-dir",
+    help="Directory with the schemas of API methods",
 )
 def cli():
-	pass
+    pass
 
 
 def main():
-	logging.basicConfig(
-		level=logging.INFO,
-		format="[%(levelname)s] %(asctime)s - %(name)s - %(message)s",
-	)
-	cli()
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(levelname)s] %(asctime)s - %(name)s - %(message)s",
+    )
+    cli()
 
 
-if __name__ == '__main__':
-	main()
+if __name__ == "__main__":
+    main()
