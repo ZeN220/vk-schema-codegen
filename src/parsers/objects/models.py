a6e3bb56b42fbc169d68094a1af0392fd2681cf8
ZeN <artaganmalsagov07@gmail.com>
2022-12-03 13:56:01 +0300

    added models for enum objects

--- src/parsers/objects/models.py
+++ src/parsers/objects/models.py
@@ -12,10 +12,50 @@ class ObjectSchema(Struct):
 
     def __str__(self):
         name = to_camel_case(self.name)
-        class_string = f"class {name}(BaseModel):\n"
+        class_string = f"class {name}(pydantic.BaseModel):\n"
         for property_ in self.properties:
             class_string += str(property_)
         class_string += "\n"
+        return class_string
+
+
+class EnumSchema(Struct):
+    type: str
+    name: str
+    description: Optional[str] = None
+
+
+class EnumStringSchema(EnumSchema):
+    enum: list[str]
+    enumNames: Optional[list[str]] = None
+    """In schema of API this name is in CamelCase"""
+
+    def __str__(self):
+        class_string = f"class {self.name}(enum.Enum):\n"
+        if self.enumNames is not None:
+            for enum, enum_name in zip(self.enum, self.enumNames):
+                enum_name = enum_name.upper().replace(" ", "_")
+                class_string += f'\t{enum_name} = "{enum}"\n'
+        else:
+            for enum in self.enum:
+                enum_name = enum.upper()
+                class_string += f'\t{enum_name} = "{enum_name}"\n'
+        class_string += "\n"
+        return class_string
+
+
+class EnumIntegerSchema(EnumSchema):
+    enum: list[int]
+    enumNames: list[str]
+    """In schema of API this name is in CamelCase"""
+    default: Optional[int] = None
+    minimum: Optional[int] = None
+
+    def __str__(self):
+        class_string = f"class {self.name}(enum.IntEnum):\n"
+        for enum, enum_name in zip(self.enum, self.enumNames):
+            enum_name = enum_name.upper().replace(" ", "_")
+            class_string += f"\t{enum_name} = {enum}\n"
         return class_string
 
 
