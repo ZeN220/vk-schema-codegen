5740cc0e732c20afe54e1fa9294f64001c285019
ZeN <artaganmalsagov07@gmail.com>
2022-12-11 11:38:05 +0300

    fix parse allOf objects

--- src/parsers/objects/schemas/all_of.py
+++ src/parsers/objects/schemas/all_of.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
-from src.fields import ReferenceField
-from src.strings import to_camel_case
+from msgspec import Struct
+
+from src.strings import REFERENCE_REGEX, to_camel_case
 
 from .base import BaseSchema
 from .object import ObjectSchema
@@ -10,7 +11,7 @@ from .object import ObjectSchema
 class AllOfObjectSchema(BaseSchema):
     object_schema: ObjectSchema
     """Last element from "allOf" object from schema"""
-    allOf: list[ReferenceField]
+    allOf: list[ReferenceAllOf]
 
     @classmethod
     def from_dict(cls, name, all_of: list[dict]) -> AllOfObjectSchema:
@@ -18,16 +19,29 @@ class AllOfObjectSchema(BaseSchema):
         object_schema = ObjectSchema.from_dict(name, object_schema["properties"])
         references = []
         for reference in all_of:
-            reference = ReferenceField(reference=reference["$ref"])
+            reference = ReferenceAllOf(reference=reference["$ref"])
             references.append(reference)
         schema = cls(name=name, object_schema=object_schema, allOf=references)
         return schema
 
     def __str__(self):
+        references = [reference.get_reference() for reference in self.allOf]
+        child_classes = ", ".join(references)
+
         name = to_camel_case(self.name)
-        child_classes = ", ".join([reference.__typehint__ for reference in self.allOf])
         class_string = f"class {name}({child_classes}):\n"
         for property_ in self.object_schema.properties:
             class_string += str(property_)
         class_string += "\n"
         return class_string
+
+
+class ReferenceAllOf(Struct):
+    reference: str
+
+    def get_reference(self) -> str:
+        reference = REFERENCE_REGEX.match(self.reference)
+        if reference is None:
+            raise ValueError(f"Invalid reference: {self.reference}")
+        result = to_camel_case(reference.group(1))
+        return result
