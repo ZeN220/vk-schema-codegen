b8543d2eb806633dd214216113f36a98ca7e8f0a
ZeN <artaganmalsagov07@gmail.com>
2022-12-11 20:05:14 +0300

    fix parse allOf objects

--- src/parsers/objects/schemas/all_of.py
+++ src/parsers/objects/schemas/all_of.py
@@ -15,7 +15,8 @@ class AllOfObjectSchema(BaseSchema):
 
     @classmethod
     def from_dict(cls, name, all_of: list[dict]) -> AllOfObjectSchema:
-        object_schema = all_of.pop(-1)
+        object_index = _get_index_object(all_of)
+        object_schema = all_of.pop(object_index)
         object_schema = ObjectSchema.from_dict(name, object_schema["properties"])
         references = []
         for reference in all_of:
@@ -45,3 +46,10 @@ class ReferenceAllOf(Struct):
             raise ValueError(f"Invalid reference: {self.reference}")
         result = to_camel_case(reference.group(1))
         return result
+
+
+def _get_index_object(all_of: list[dict]) -> int:
+    for index, element in enumerate(all_of):
+        if element.get("properties") is not None:
+            return index
+    raise ValueError("Invalid schema: no object found in 'allOf' list.")
