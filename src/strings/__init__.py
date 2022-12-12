912b5028133fca72b253f3721ba9eb00683de739
ZeN <artaganmalsagov07@gmail.com>
2022-12-12 19:41:36 +0300

    move "to_python_types" to converter.py

--- src/strings/__init__.py
+++ src/strings/__init__.py
@@ -1,5 +1,4 @@
-from .converter import to_camel_case
+from .converter import to_camel_case, to_python_types
 from .reference import get_reference
-from .types import to_python_types
 
 __all__ = ["to_camel_case", "get_reference", "to_python_types"]
--- src/strings/converter.py
+++ src/strings/converter.py
@@ -3,3 +3,15 @@ def to_camel_case(snake_str: str) -> str:
     for word in snake_str.split("_"):
         result += word[0].upper() + word[1:]
     return result
+
+
+def to_python_types(types: list[str]) -> list[str]:
+    result = []
+    for type_field in types:
+        if type_field == "string":
+            result.append("str")
+        elif type_field == "integer":
+            result.append("int")
+        else:
+            raise ValueError(f"Unknown union type: {type_field}")
+    return result
