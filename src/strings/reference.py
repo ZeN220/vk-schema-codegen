1db2c5580cc95c6959eab61eb1b3552884ffb782
ZeN <artaganmalsagov07@gmail.com>
2022-12-15 17:58:08 +0300

    added "validate_order_references" function

--- src/strings/reference.py
+++ src/strings/reference.py
@@ -5,8 +5,13 @@ from .converter import to_camel_case
 REFERENCE_REGEX = re.compile(r"\.\./[a-zA-Z]+/objects\.json#/definitions/([a-zA-Z_]+)")
 
 
-def get_reference(reference: str) -> str:
+def parse_reference(reference: str) -> str:
     result = REFERENCE_REGEX.match(reference)
     if result is None:
         raise ValueError(f"Invalid reference: {result}")
-    return to_camel_case(result.group(1))
+    return result.group(1)
+
+
+def get_reference(reference: str) -> str:
+    reference = parse_reference(reference)
+    return to_camel_case(reference)
