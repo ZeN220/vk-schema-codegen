b4a601d28f76cfbe78d8f619de500d1b09e89ef1
ZeN <artaganmalsagov07@gmail.com>
2022-12-28 18:27:52 +0300

    fix __all__ list

--- src/cli/commands/__init__.py
+++ src/cli/commands/__init__.py
@@ -1,4 +1,4 @@
 from .objects import command_generate_objects
 from .responses import command_generate_responses
 
-__all__ = ["command_generate_objects", command_generate_responses]
+__all__ = ["command_generate_objects", "command_generate_responses"]
