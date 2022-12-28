6374e7482f30b1b3d85970058c2080e32ec59131
ZeN <artaganmalsagov07@gmail.com>
2022-12-28 17:58:43 +0300

    some refactoring test_cli.py

--- tests/test_cli/test_cli.py
+++ tests/test_cli/test_cli.py
@@ -1,13 +1,20 @@
+import click
 import pytest
 from click.testing import CliRunner
 
 from src.cli.cli import cli
 
 
+@click.command("test_command")
+def cli_test_command():
+    pass
+
+
 def test_cli(tmp_path_factory: pytest.TempPathFactory):
     input_dir = tmp_path_factory.mktemp("input")
     output_dir = tmp_path_factory.mktemp("output")
     runner = CliRunner()
+    cli.add_command(cli_test_command)
     result = runner.invoke(
         cli,  # noqa
         [
@@ -15,8 +22,7 @@ def test_cli(tmp_path_factory: pytest.TempPathFactory):
             str(input_dir),
             "--output-dir",
             str(output_dir),
+            "test_command",
         ],
     )
-    # Because we don't have any commands, it should fail
-    assert result.exit_code == 2
-    assert "Error: Missing command." in result.output
+    assert result.exit_code == 0
