516eb4fa6e3063ade07ffb15986359ba3fcc02bd
ZeN <artaganmalsagov07@gmail.com>
2022-12-30 15:30:24 +0300

    fix typo

--- src/cli/commands/generate.py
+++ src/cli/commands/generate.py
@@ -4,7 +4,7 @@ from src.cli.config import Config
 from src.codegen import generate_objects, generate_responses
 
 
-@click.command("generate", help="Generate code from API schema")
+@click.command("generate", help="Generate code for all entities from API schema")
 @click.pass_obj
 def command_generate(config: Config):
     generate_objects(input_dir=config.input_dir, output_dir=config.output_dir)
