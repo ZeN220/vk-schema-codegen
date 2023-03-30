99b9f13bda75a2a1fe6b5ccb96564dcd86055de8
scripthound <grinkiko@gmail.com>
2023-03-30 17:37:01 +0300

    refactored properties

--- src/properties/properties.py
+++ src/properties/properties.py
@@ -15,6 +15,74 @@ from .string_enum import StringEnumProperty
 from .union import UnionProperty
 
 
+PROPERTIES_TYPES = {"$ref", "oneOf", "patternProperties", "type"}
+
+
+def extract_property_type(item: dict) -> str:
+    prop = list(filter(lambda x: x in PROPERTIES_TYPES, item.keys()))[0]
+    return prop
+
+
+class TypesHandler:
+    def __init__(self, item_type):
+
+        # dollar sign is not allowed in python variable name
+        if item_type == "$ref":
+            self.type = "ref"
+        else:
+            self.type = item_type
+
+    def handle_item_type(self, item: dict, name: str) -> BaseProperty:
+        method = getattr(self, f"build_from_{self.type}")
+        return method(item, name)
+
+    def build_from_ref(self, item: dict, name: str) -> BaseProperty:
+        copy_item = item.copy()
+        ref = copy_item.pop("$ref")
+        reference = get_reference(ref)
+        return ReferenceProperty(name=name, reference=reference, **copy_item)
+
+    def build_from_oneOf(self, item: dict, name: str) -> BaseProperty:
+        copy_item = item.copy()
+        one_of_elements = copy_item.pop("oneOf")
+        one_of = [get_property_from_dict(item, name) for item in one_of_elements]
+        return OneOfProperty(name=name, oneOf=one_of, **copy_item)
+
+    def build_from_patternProperties(self, item: dict, name: str) -> BaseProperty:
+        copy_item = item.copy()
+        pattern_properties = copy_item.pop("patternProperties")
+        pattern_properties = {
+            key: get_property_from_dict(value, name) for key, value in pattern_properties.items()
+        }
+        return PatternProperty(name=name, patternProperties=pattern_properties, **copy_item)
+
+    def build_from_type(self, item: dict, name: str) -> BaseProperty:
+        property_type = item.get("type")
+        if isinstance(property_type, list):
+            return UnionProperty(name=name, **item)
+        if property_type == "array":
+            copy_item = item.copy()
+            array_items = copy_item.pop("items")
+            items = get_property_from_dict(array_items, name)
+            return ArrayProperty(name=name, items=items, **copy_item)
+        if property_type == "object":
+            return DictProperty(name=name, **item)
+        if property_type == "integer":
+            return IntegerProperty(name=name, **item)
+        if property_type == "number":
+            return FloatProperty(name=name, **item)
+        if property_type == "boolean":
+            return BooleanProperty(name=name, **item)
+        if property_type == "string":
+            # Some properties with the type "string" may have the field "minimum".
+            # I do not know what it is for, so it is simply deleted
+            copy_item = item.copy()
+            copy_item.pop("minimum", None)
+            return StringProperty(name=name, **copy_item)
+        else:
+            raise ValueError(f"Unknown property type: {property_type}")
+
+
 def get_property_from_dict(item: dict, name: str) -> BaseProperty:
     """
     Some properties have a nested properties, which are not defined names.
@@ -23,47 +91,10 @@ def get_property_from_dict(item: dict, name: str) -> BaseProperty:
     :param name: name of property
     :return: property as BaseProperty class
     """
-    if item.get("$ref") is not None:
-        copy_item = item.copy()
-        ref = copy_item.pop("$ref")
-        reference = get_reference(ref)
-        return ReferenceProperty(name=name, reference=reference, **copy_item)
-    if item.get("oneOf") is not None:
-        copy_item = item.copy()
-        one_of_elements = copy_item.pop("oneOf")
-        one_of = [get_property_from_dict(item, name) for item in one_of_elements]
-        return OneOfProperty(name=name, oneOf=one_of, **copy_item)
-    if item.get("patternProperties") is not None:
-        copy_item = item.copy()
-        pattern_properties = copy_item.pop("patternProperties")
-        pattern_properties = {
-            key: get_property_from_dict(value, name) for key, value in pattern_properties.items()
-        }
-        return PatternProperty(name=name, patternProperties=pattern_properties, **copy_item)
+    property_type = extract_property_type(item)
+    types_handler = TypesHandler(property_type)
 
-    property_type = item.get("type")
-    if isinstance(property_type, list):
-        return UnionProperty(name=name, **item)
-    if property_type == "array":
-        copy_item = item.copy()
-        array_items = copy_item.pop("items")
-        items = get_property_from_dict(array_items, name)
-        return ArrayProperty(name=name, items=items, **copy_item)
-    if property_type == "object":
-        return DictProperty(name=name, **item)
-    if property_type == "integer":
-        return IntegerProperty(name=name, **item)
-    if property_type == "number":
-        return FloatProperty(name=name, **item)
-    if property_type == "boolean":
-        return BooleanProperty(name=name, **item)
-    if property_type == "string":
-        # Some properties with the type "string" may have the field "minimum".
-        # I do not know what it is for, so it is simply deleted
-        copy_item = item.copy()
-        copy_item.pop("minimum", None)
-        return StringProperty(name=name, **copy_item)
-    raise ValueError(f"Unknown property type: {property_type}")
+    return types_handler.handle_item_type(item, name)
 
 
 def get_enum_property_from_dict(item: dict, name: str, typehint: str) -> BaseProperty:
